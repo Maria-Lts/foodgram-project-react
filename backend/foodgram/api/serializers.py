4dfa976d10a77fd442e595d2d7214e5ba2992521
Мария Луцюк <mashaverstuckowa@yandex.ru>
2024-04-15 15:08:46 +0300

    serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -163,18 +163,6 @@ class RecipeWriteSerializer(serializers.ModelSerializer):
             for ingredient in ingredients
         ]
         IngredientAmount.objects.bulk_create(create_ingredients)
-        # for ingredient_list in ingredient:
-        #     amount = ingredient_list['amount']
-        #     ingredient_id = ingredient_list['id']
-        #     raise ValueError(f'{ingredient}') 
-        #     if ingredient_id:
-        #         ingredient = get_object_or_404(Ingredient, pk=ingredient_id)
-        #         ingredient_amount = IngredientAmount.objects.create(
-        #             recipe=recipe,
-        #             ingredient=ingredient,
-        #             amount=amount
-        #         )
-        #         ingredient_amount.save()
 
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
