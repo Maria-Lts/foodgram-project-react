5e66dd4b24e5dba2c3eeef1f45238c6aff60cff3
Мария Луцюк <mashaverstuckowa@yandex.ru>
2024-05-11 19:20:46 +0300

    Serializers

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -95,8 +95,7 @@ class IngredientsAmountSerializer(serializers.ModelSerializer):
 
 
 class IngredientAmountWriteSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField()
-    # source='ingredient.id')
+    id = serializers.IntegerField(source='ingredient.id')
     # id = serializers.PrimaryKeyRelatedField(
     #     queryset=Ingredient.objects.all(),
     #     source='ingredient.id')
